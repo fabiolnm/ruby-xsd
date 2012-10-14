adcfac59f93376eaea794b04531d2d9b9dd5b1a6
Fábio Luiz Nery de Miranda <fabio@miranti.net.br>
2012-10-14 05:19:26 -0300

    Simple elements simplest parsing

--- lib/ruby-xsd.rb
+++ lib/ruby-xsd.rb
@@ -1,7 +1,47 @@
 require "ruby-xsd/version"
+require "nokogiri"
 
-module Ruby
-  module Xsd
-    # Your code goes here...
+class RubyXsd
+  XMLSchemaNS = "http://www.w3.org/2001/XMLSchema"
+
+  def self.models_from xsd_definitions
+    doc = Nokogiri::XML xsd_definitions
+
+    schema = doc.children.first
+    raise "Invalid XMLSchema root" if schema.name != "schema"
+    raise "Missing XMLSchema namespace" if schema.namespace.nil?
+    raise "Wrong XMLSchema namespace" unless is_xml_schema_node schema
+
+    schema.children.each { |node| make_definition node }
+  end
+
+  def self.make_definition node
+    if is_element_node node
+      attrs = node.attributes.to_hash
+      define_accessor attrs["name"]
+    end
+  end
+
+  private
+  def self.namespace_of node
+    node.namespace.href
+  end
+
+  def self.is_xml_schema_node node
+    namespace_of(node) == XMLSchemaNS
+  end
+
+  def self.is_element_node node
+    is_xml_schema_node node and node.name == "element"
+  end
+
+  def self.define_accessor name
+    define_method name do
+      instance_variable_get "@#{name}"
+    end
+
+    define_method "#{name}=" do |value|
+      instance_variable_set "@#{name}", value
+    end
   end
 end
--- test/ruby-xsd/test.rb
+++ test/ruby-xsd/test.rb
@@ -1,3 +1,39 @@
-describe "Ruby-Xsd" do
-  it "must have tests"
+describe RubyXsd do
+  describe "XMLSchema root validation" do
+    it "rejects non-schema root" do
+      ex = assert_raises RuntimeError do
+        RubyXsd.models_from "<non-xs></non-xs>"
+      end
+      assert_equal "Invalid XMLSchema root", ex.message
+    end
+
+    it "rejects schema root without XMLSchema namespace" do
+      ex = assert_raises RuntimeError do
+        RubyXsd.models_from "<schema></schema>"
+      end
+      assert_equal "Missing XMLSchema namespace", ex.message
+    end
+
+    it "rejects schema with wrong XMLSchema namespace" do
+      ex = assert_raises RuntimeError do
+        RubyXsd.models_from "<xs:schema xmlns:xs='wrong'></xs:schema>"
+      end
+      assert_equal "Wrong XMLSchema namespace", ex.message
+    end
+  end
+
+  let(:schema) {
+    "<xs:schema xmlns:xs='#{RubyXsd::XMLSchemaNS}'>%s</xs:schema>"
+  }
+
+  describe "simple elements" do
+    let(:template) {
+      schema % "<xs:element name='%s' />"
+    }
+
+    it "defines object attributes" do
+      RubyXsd.models_from template % [ "xsd_attr" ]
+      RubyXsd.new.must_respond_to :xsd_attr
+    end
+  end
 end
